5155683a3338d928f5993b3247c06b26cb15a667
ParishBen <ParishBen13@gmail.com>
2020-12-05 17:11:46 -0500

    commit tree 94f330048df9d2b2ba3dc1af89a25f5a135ed2ac

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arrObj){
+let winningTeam = arrObj.find( winner => winner.result === "W");
+if (!!winningTeam){
+   return winningTeam.year;
+}
+  return undefined
+ 
+}
+
+
+// superbowlWin = (record) => {
+//   let result = record.find( record => record.result === "W" )
+//   return !!result ? result.year : undefined
+// }
