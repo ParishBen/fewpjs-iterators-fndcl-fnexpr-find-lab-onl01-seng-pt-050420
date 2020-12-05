db7ae8f3c3193b145ef40c971aae0dc99b218f99
ParishBen <ParishBen13@gmail.com>
2020-12-05 17:15:58 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arrObj){
+let winningTeam = arrObj.find( winner => winner.result === "W");
+if (winningTeam){
+   return winningTeam.year;
+}
+  return undefined
+ 
+}
